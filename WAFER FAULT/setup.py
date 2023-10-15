1d60d0afce0d59d11fa0b8ad2bd35cb53c3e9059
Asteekraj <asteek00304@gmail.com>
2023-10-15 19:06:35 +0530

    first commit

---
+++ WAFER FAULT/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wafer_fault",
+    version="0.0.1",
+    author="Asteek_Raj",
+    author_email="asteek00304@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
